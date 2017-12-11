7dacc6f842e07864e18c2998d85685b80a5c165d
Adrian Tilita <adrian@tilita.ro>
2017-12-11 14:47:20 +0200

    Fix cros-compile, add binaries

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,9 +1,10 @@
+cd src
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
         echo "Building for \033[1;33m$GOOS-$GOARCH\033[0m"
         start=`date +%s`
         rm -rf build/needle-url-$GOOS-$GOARCH
-        CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o build/needle-url-$GOOS-$GOARCH src/main.go
+        CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o ../build/needle-url-$GOOS-$GOARCH
         end=`date +%s`
         runtime=$((end-start))
         echo "Build for \033[1;33m$GOOS-$GOARCH\033[0m in $runtime seconds"
